3b855fc8a1a6f0890ea23a3ff8bf04c4e9052b26
Laura Vergara <laura.vergarar@upb.edu.co>
2021-02-16 15:12:29 -0500

    Add Range Enemy Scripts

---
+++ PKFC/Assets/Scripts/Enemy_Range_attack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Range_attack : MonoBehaviour
+{
+    private float timeBtwShots;
+    [SerializeField]
+    private float starTimeBtwShots;
+    [SerializeField]
+    private GameObject projectile;
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = starTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = starTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
