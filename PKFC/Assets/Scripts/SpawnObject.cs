21afb56c5f9d3c2d679bb8d855be338cba10ae96
Laura Vergara <laura.vergarar@upb.edu.co>
2021-02-22 16:00:38 -0500

    LR room design & spawn
    saving in case laptop explodes

---
+++ PKFC/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject oceanTile;
+
+    private void Start()
+    {
+        Instantiate(oceanTile, transform.position, Quaternion.identity);
+    }
+}
