5cc0e7db8f768e1e2a9790203762034383cbc370
Marcelo Alvarenga Tamayo <Marcelo.alvarenga@upb.edu.co>
2021-03-06 20:30:14 -0500

    Tutorial changes + Boss room

--- PKFC/Assets/Scripts/RoomTemplates.cs
+++ PKFC/Assets/Scripts/RoomTemplates.cs
@@ -13,5 +13,26 @@ public class RoomTemplates : MonoBehaviour
 
     public List<GameObject> rooms;
 
+    public float waitTime;
+    private bool spawnedBoss;
+    public GameObject boss;
 
+    private void Update()
+    {
+        if(waitTime <= 0 && spawnedBoss == false)
+        {
+            for (int i = 0; i < rooms.Count; i++)
+            {
+                if(i == rooms.Count - 1)
+                {
+                    Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+                    spawnedBoss = true;
+                }
+            }
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
+    }
 }
