8bcc0919568d64e9dfb08d79e0e93ca5b841652f
Bleysiker <henry3197@hotmail.com>
2021-03-25 17:38:14 -0500

    new NPC on the store :D

--- PKFC/Assets/Scripts/Patrol.cs
+++ PKFC/Assets/Scripts/Patrol.cs
@@ -9,14 +9,22 @@ public class Patrol : MonoBehaviour
     private float waitTime;
     [SerializeField]
     private float startWaitTime;
-
+    
     [SerializeField]
     private Transform[] moveSpots;
     private int randomSpot;
-     
-    
+
+    private Transform actualposition;
+    private float previousPosition;
+    private Animator anim;
+    private bool moving;
+
     void Start()
     {
+        moving = true;
+        anim = GetComponent<Animator>();
+        actualposition = GetComponent<Transform>();
+        previousPosition = actualposition.position.y;
         waitTime = startWaitTime;
         randomSpot = Random.Range(0,moveSpots.Length); 
     }
@@ -24,19 +32,49 @@ public class Patrol : MonoBehaviour
     
     void Update()
     {
+        moving = true;
+        actualposition = transform;
         transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        ChangeAnimations(actualposition,previousPosition,moving);
 
         if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
         {
+            moving = false;
+            previousPosition = actualposition.position.y;
+            ChangeAnimations(actualposition, previousPosition,moving);
             if (waitTime <= 0)
             {
                 randomSpot = Random.Range(0, moveSpots.Length);
-                waitTime = startWaitTime;   
+                waitTime = startWaitTime;
+                  
             }
             else
             {
+
                 waitTime -= Time.deltaTime;
             }
         }
     }
-}
+    void ChangeAnimations(Transform actualPosition,float previousPosition,bool moving)
+    {
+        if (previousPosition > actualPosition.position.y && moving==true)
+        {
+            anim.SetBool("movingUp", false);
+            anim.SetBool("movingDown", true);
+            anim.SetBool("moving", true);
+        }
+        else if (previousPosition < actualPosition.position.y && moving==true)
+        {
+            anim.SetBool("movingUp", true);
+            anim.SetBool("movingDown", false);
+            anim.SetBool("moving", true);
+        }
+        else if(previousPosition == actualPosition.position.y && moving==false){
+            anim.SetBool("movingUp", false);
+            anim.SetBool("movingDown", false);
+            anim.SetBool("moving", false);
+        }
+            
+        }
+    }
+
