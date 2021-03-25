10d378d72fb2d9867b52b7f359caec1d13561b09
Laura Vergara <laura.vergarar@upb.edu.co>
2021-03-25 08:33:19 -0500

    States

---
+++ PKFC/Assets/Scripts/Poisoned.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poisoned : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Poisoned!");
+
+        }
+    }
+}
