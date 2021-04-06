09085d8b8788ec4c9123f88d3a2ad60152e98a68
Laura Vergara <laura.vergarar@upb.edu.co>
2021-04-06 06:50:30 -0500

    Blind & Burn states

--- PKFC/Assets/Scripts/States/Blinded.cs
+++ PKFC/Assets/Scripts/States/Blinded.cs
@@ -1,15 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Blinded : MonoBehaviour
 {
+    public HurtEnemy hurtEnemy;
+    public Collider2D player;
+    public GameObject burnImage;
+
+    [SerializeField] FlashImage flashImage = null;
+    [SerializeField] Color _newColor = Color.green;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+
+
         if (other.tag == "Player")
         {
+            BlindPlayer();
+
+            flashImage.StartFlash(1f, .3f, _newColor);
+
             Debug.Log("Blinded!");
 
         }
+    }
+
+    public void BlindPlayer()
+    {
+        StartCoroutine("Blind");
+
+    }
+
+    IEnumerator Blind()
+    {
+        hurtEnemy.damageToGive -= 10;
+        burnImage.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        hurtEnemy.damageToGive += 10;
+        burnImage.SetActive(false);
     }
 }
--- PKFC/Assets/Scripts/States/Burned.cs
+++ PKFC/Assets/Scripts/States/Burned.cs
@@ -1,15 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Burned : MonoBehaviour
 {
+    public HealthSystem health;
+    public Collider2D player;
+    public GameObject burnImage;
+
+    [SerializeField] FlashImage flashImage = null;
+    [SerializeField] Color _newColor = Color.red;
+
+    public List<int> burnTickTimers = new List<int>();
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            BurnPlayer(4);
+
             Debug.Log("Burned!");
 
         }
+    }
+
+    public void BurnPlayer(int ticks)
+    {
+        if (burnTickTimers.Count <= 0)
+        {
+            burnTickTimers.Add(ticks);
+            StartCoroutine(Burn());
+        }
+        else
+        {
+            burnTickTimers.Add(ticks);
+        }
+    }
+
+    IEnumerator Burn()
+    {
+        while (burnTickTimers.Count > 0)
+        {
+            for (int i = 0; i < burnTickTimers.Count; i++)
+            {
+                burnTickTimers[i]--;
+            }
+            health.lasaluddeljugador -= 5;  //CHELO AYUDA POR FAVOR
+            burnTickTimers.RemoveAll(i => i == 0);
+
+            yield return new WaitForSeconds(0.75f);
+;       }
     }
 }
