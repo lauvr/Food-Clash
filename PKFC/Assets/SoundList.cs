9aa86d7ca8721c5ef6d2880ef2031fb124b2fbce
Bleysiker <henry3197@hotmail.com>
2021-05-24 03:37:04 -0500

    :v

---
+++ PKFC/Assets/SoundList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundList : MonoBehaviour
+{
+    public GameObject soundtracks;
+    // Start is called before the first frame update
+}
--- PKFC/Assets/Videoactive.cs
+++ PKFC/Assets/Videoactive.cs
@@ -1,32 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class Videoactive : MonoBehaviour
 {
     public GameObject videoplayer;
     public int tts;
+    public GameObject music;
+    public GameObject boss;
+    private bool firsttime;
+    
     // Start is called before the first frame update
     void Start()
     {
+        
         videoplayer.SetActive(false);
+        firsttime = true;
+        
     }
 
     void OnTriggerEnter2D (Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")&&firsttime==true)
         {
             Debug.Log(":v");
+
+            music.GetComponent<AudioSource>().volume=0;
             videoplayer.SetActive(true);
             Destroy(videoplayer, tts);
-            //StartCoroutine(Desactivatepls());
             
+            StartCoroutine(Desactivatepls());
+            firsttime = false;
         }
     }
-   /* IEnumerator Desactivatepls()
+    IEnumerator Desactivatepls()
     {
         
         yield return new WaitForSeconds(6f);
-        videoplayer.SetActive(false);
-    }*/
+        boss.GetComponent<AudioSource>().volume = .4f;
+    }
 }
